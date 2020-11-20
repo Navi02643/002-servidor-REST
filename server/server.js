fe8c1588a48c4f6396d38514735d260bcbeedf8f
Navi02643 <karlos123jun@hotmail.com>
2020-11-20 15:57:26 -0600

    Conexión lista a base de datos

--- server/server.js
+++ server/server.js
@@ -1,64 +1,80 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.send('<h1>Bienvenido a mi servidor REST (localhost)</h1>');
+app.get("/", function (req, res) {
+  res.send("<h1>Bienvenido a mi servidor REST (localhost)</h1>");
 });
 
-app.get('/usuario', function(req, res) {
-    res.json({
-        ok: 200,
-        mesaje: 'usuarios consultados con exito'
+app.get("/usuario", function (req, res) {
+  res.json({
+    ok: 200,
+    mesaje: "usuarios consultados con exito",
+  });
+});
+
+app.post("/usuario", function (req, res) {
+  let nombre = req.body.nombre;
+  let body = req.body;
+
+  if (nombre === undefined) {
+    res.status(400).json({
+      ok: 400,
+      mensaje: "favor de enviar el valor del nombre",
     });
+  }
+
+  res.json({
+    ok: 200,
+    mensaje: "usuario insertado con exito",
+    nombre: nombre,
+    body: body,
+  });
 });
 
-app.post('/usuario', function(req, res) {
-    let nombre = req.body.nombre;
-    let body = req.body;
+app.put("/usuario/:id/:nombre", function (req, res) {
+  let id = req.params.id;
+  let nombre = req.params.nombre;
 
-    if (nombre === undefined) {
-        res.status(400).json({
-            ok: 400,
-            mensaje: 'favor de enviar el valor del nombre'
-        });
-    }
-
-    res.json({
-        ok: 200,
-        mensaje: 'usuario insertado con exito',
-        nombre: nombre,
-        body: body
-    });
+  res.json({
+    ok: 200,
+    mesaje: "usuario actualizado con exito",
+    id: id,
+    nombre: nombre,
+  });
 });
 
-app.put('/usuario/:id/:nombre', function(req, res) {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
+app.delete("/usuario/:id", function (req, res) {
+  let id = req.params.id;
 
-    res.json({
-        ok: 200,
-        mesaje: 'usuario actualizado con exito',
-        id: id,
-        nombre: nombre
-    });
+  res.json({
+    ok: 200,
+    mensaje: "usuario eliminado correctamente",
+    id: id,
+  });
 });
 
-app.delete('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        ok: 200,
-        mensaje: 'usuario eliminado correctamente',
-        id: id
-    });
-});
+mongoose.connect(
+  "mongodb://localhost:27017/cafeteria",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err, res) => {
+    // sql 3306
+    if (err) throw err;
+    console.log("Base de datos ONLINE");
+  }
+);
 
 app.listen(process.env.PORT, () => {
-    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
+  console.log("El servidor esta en linea por el puerto ", process.env.PORT);
 });
