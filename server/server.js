31132bd2bc65ba4981327b78619d4293631759e0
Navi02643 <karlos123jun@hotmail.com>
2020-12-07 18:55:57 -0600

    Se agrego la funcion cors

--- server/server.js
+++ server/server.js
@@ -3,10 +3,20 @@ require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Habilitar CORS
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    next();
+});
 
 app.get("/", function (req, res) {
   res.send("<h1>Bienvenido a mi servidor REST</h1>");
